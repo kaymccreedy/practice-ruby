7a143d9c28814df743e7ab5761d6ff88080c9cc6
Kay McCreedy <thiskieranmccreedy@gmail.com>
2022-06-21 14:59:20 -0400

    adds three items

---
+++ store_item.rb
@@ -0,0 +1,3 @@
+item1 = { make: "Fender", model: "Stratocaster", color: "white", price: 800}
+item2 = { make: "Gibson", model: "Les Paul", color: "black", price: 1200}
+item3 = { :make => "Gibson", :model => "Firebird", :color => "red", :price => 1000}
