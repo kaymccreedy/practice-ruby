53c23a66262fe288d903efe18d089d215d3383b3
Kay McCreedy <thiskieranmccreedy@gmail.com>
2022-06-22 12:44:52 -0400

    updates initialize to use hash

--- store_item.rb
+++ store_item.rb
@@ -7,11 +7,11 @@ class Item
   attr_reader :make, :model, :color, :price
   attr_writer :color, :price
 
-  def initialize(make, model, color, price)
-    @make = make
-    @model = model
-    @color = color
-    @price = price
+  def initialize(options)
+    @make = options[:make]
+    @model = options[:model]
+    @color = options[:color]
+    @price = options[:price]
   end
 
   def print_info
@@ -24,12 +24,14 @@ class Item
 
 end
 
-item1 = Item.new("Fender", "Stratocaster", "white", 800)
-item2 = Item.new("Gibson", "Les Paul", "black", 1200)
-item3 = Item.new("Gibson", "Firebird", "red", 1000)
+item1 = Item.new( make: "Fender", model: "Stratocaster", color: "white", price: 800 )
+item2 = Item.new( make: "Gibson", model: "Les Paul", color: "black", price: 1200 )
+item3 = Item.new( make: "Gibson", model: "Firebird", color: "red", price: 1000 )
 
 p item1
 item1.print_info
+item2.print_info
+item3.print_info
 
 item1.raise_price
 item1.print_info
@@ -38,7 +40,7 @@ item1.price = 900
 item1.color = "green"
 item1.print_info
 
-item4 = Item.new("Squire", "Telecaster", "blue", 500)
+item4 = Item.new( make: "Squire", model: "Telecaster", color: "blue", price: 500 )
 5.times do
   item4.raise_price
   item4.print_info
