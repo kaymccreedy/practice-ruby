9f5e9d892dd1fdf6870de320c620885107af89d0
Kay McCreedy <thiskieranmccreedy@gmail.com>
2022-06-22 12:48:10 -0400

    removed old hash-based code

--- store_item.rb
+++ store_item.rb
@@ -1,7 +1,3 @@
-item1 = { make: "Fender", model: "Stratocaster", color: "white", price: 800}
-item2 = { make: "Gibson", model: "Les Paul", color: "black", price: 1200}
-item3 = { :make => "Gibson", :model => "Firebird", :color => "red", :price => 1000}
-
 class Item
 
   attr_reader :make, :model, :color, :price
